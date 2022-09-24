a730e586b123a50c80392ea995ab280bacfc8cab
commanderxa <art.ostrikov@gmail.com>
2022-09-24 21:23:01 +0600

    Area of accuracy works as intended

--- src/app.js
+++ src/app.js
@@ -15,10 +15,19 @@ let mapOptions = {
     "center": { lat: 51.5072, lng: 0.1276 },
     "altitude": 20,
     "mapId": process.env.MAP_ID,
-}
+};
+
+let areaOptions = {
+    "horizontalAccuracy": 1,
+    "verticalAccuracy": 1,
+};
 
 let points = [];
-const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
+const material = new THREE.LineBasicMaterial({ color: 0x0000ff, linewidth: 3 });
+
+function latLngAltToMeters(lat, lng, alt) {
+    return [lat * 111.32, lng * 40075 * Math.cos(lat) / 360, alt * 0.3048];
+}
 
 async function initMap() {
     const mapDiv = document.getElementById("map");
@@ -26,7 +35,6 @@ async function initMap() {
     await apiLoader.load();
     return new google.maps.Map(mapDiv, mapOptions);
 }
-
 
 function initWebGLOverlayView(map) {
     let scene, renderer, camera, loader;
@@ -48,6 +56,10 @@ function initWebGLOverlayView(map) {
         dracoLoader.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/');
         loader.setDRACOLoader(dracoLoader);
 
+        let geometry = new THREE.BufferGeometry().setFromPoints(points);
+        let line = new THREE.Line(geometry, material);
+        scene.add(line);
+
         const source = "./models/ruby.gltf";
         loader.load(
             source,
@@ -63,15 +75,37 @@ function initWebGLOverlayView(map) {
             }
         );
 
+        // location data
         var data = require('./data/data.json');
         const dataList = document.getElementById("data-list");
 
+        // initial setup of the center and area
         mapOptions.center = {
             lat: data[0][0].Latitude,
             lng: data[0][0].Longitude,
         };
         mapOptions.altitude = data[0][0].Altitude;
+        areaOptions.horizontalAccuracy = data[0][0]['Horizontal accuracy'];
+        areaOptions.verticalAccuracy = data[0][0]['Vertical accuracy'];
 
+        // solid area
+        let mesh = new THREE.Mesh(
+            new THREE.SphereGeometry(1, 20, 20),
+            new THREE.MeshBasicMaterial({ color: 0x0000FF, wireframe: false, transparent: true, opacity: 0.1 })
+        );
+        mesh.rotation.x = Math.PI / 2;
+        scene.add(mesh);
+
+        // wireframe area
+        let mesh2 = new THREE.Mesh(
+            new THREE.SphereGeometry(1, 20, 20),
+            new THREE.MeshBasicMaterial({ color: 0x0000FF, wireframe: true, transparent: true, opacity: 0.1 })
+        );
+        mesh2.applyMatrix4(new THREE.Matrix4().makeScale(areaOptions.horizontalAccuracy, areaOptions.verticalAccuracy, areaOptions.horizontalAccuracy));
+        mesh2.rotation.x = Math.PI / 2;
+        scene.add(mesh2);
+
+        // showing the data
         for (let i = 0; i < data.length; i++) {
             const dataListItem = document.createElement("option");
             const dataListItemPicture = document.createElement("div");
@@ -90,28 +124,58 @@ function initWebGLOverlayView(map) {
             dataListItemTitle.innerHTML = "<b>Dev" + (i + 1) + "</b> " + data[i][0].Identifier;
             dataListItemText.innerHTML = "; " + data[i][0].Activity;
 
-            dataList.addEventListener('change', function(event) {
+            dataList.addEventListener('change', function (event) {
                 let selectedValue = event.target.value;
                 points = [];
 
-                // Update the center position
+                // update the center position
                 mapOptions.center = {
                     lat: data[selectedValue][0].Latitude,
                     lng: data[selectedValue][0].Longitude,
                 }
                 mapOptions.altitude = data[selectedValue][0].Altitude;
-                map.setCenter(mapOptions.center);
+                map.setCenter(mapOptions.center, 50);
 
+                areaOptions.horizontalAccuracy = data[selectedValue][0]['Horizontal accuracy'];
+                areaOptions.verticalAccuracy = data[selectedValue][0]['Vertical accuracy'];
+
+                // rescaling the area
+                mesh.scale.x = mesh2.scale.x = 0;
+                mesh.scale.y = mesh2.scale.y = 0;
+                mesh.scale.z = mesh2.scale.z = 0;
+
+                mesh.scale.x = mesh2.scale.x = areaOptions.horizontalAccuracy;
+                mesh.scale.y = mesh2.scale.y = areaOptions.verticalAccuracy;
+                mesh.scale.z = mesh2.scale.z = areaOptions.horizontalAccuracy;
+
+                // check wheteher there is a trajectory and add points if is
                 if (data[selectedValue][1] !== undefined || data[selectedValue][1] !== null) {
                     for (let [key, value] of Object.entries(data[selectedValue])) {
-                        points.push(new THREE.Vector3(value.Latitude, value.Longitude, value.Altitude));
+
+                        let [latOrigin, lngOrigin, altOrigin] = latLngAltToMeters(
+                            mapOptions.center.lat,
+                            mapOptions.center.lng,
+                            mapOptions.altitude,
+                        );
+                        let [lat, lng, alt] = latLngAltToMeters(
+                            value.Latitude,
+                            value.Longitude,
+                            value.Altitude
+                        );
+                        points.push(
+                            new THREE.Vector3(
+                                latOrigin - lat, lngOrigin - lng, altOrigin - alt
+                            )
+                        );
                     }
                 }
-                console.log(points);
-                let geometry = new THREE.BufferGeometry().setFromPoints(points);
-                let line = new THREE.Line(geometry, material);
 
-                scene.add(line);
+                // drawing trajectory
+                geometry = new THREE.BufferGeometry().setFromPoints(points);
+                line = new THREE.Line(geometry, material);
+
+                console.log(points);
+
                 webGLOverlayView.requestRedraw();
             });
 
@@ -147,12 +211,11 @@ function initWebGLOverlayView(map) {
             altitude: mapOptions.altitude,
         }
 
-        console.log(points);
-
         const matrix = transformer.fromLatLngAltitude(latLngAltitudeLiteral);
         camera.projectionMatrix = new THREE.Matrix4().fromArray(matrix);
 
-     
+        // Request a redraw and render the scene.
+        webGLOverlayView.requestRedraw();
         renderer.render(scene, camera);
 
         // always reset the GL state
